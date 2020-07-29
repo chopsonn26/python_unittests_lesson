c7182bfb6d693ae7e0a187d7b15303c687991dbf
Esenalieva Cholpon <esenaliev.cholpon@gmail.com>
2020-07-29 20:59:44 +0600

    Added new test for plus fuction

--- tests/test_plus.py
+++ tests/test_plus.py
@@ -8,3 +8,6 @@ class PlusFunctionTestCase(TestCase):
 
     def test_1555_plus_445is_2000(self):
         self.assertEqual(2000,plus(1555,445))
+
+    def test_100_plus_100is_200(self):
+        self.assertEqual(200,plus(100,100))
